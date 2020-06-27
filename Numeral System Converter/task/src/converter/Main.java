a4fc63fa460be5f4c244025d3bbfe1463b5d608b
Maxim <maximorechov@yandex.ru>
2020-06-27 21:29:50 +0600

    at this stage of implementation, the program displays the last values ​​in octal format

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int decimal = 50;//sc.nextInt();
+     /*   Scanner sc = new Scanner(System.in);
+       int decimal = sc.nextInt();
         String s = "";
-        System.out.print(decimal + " = 0b");
+        //System.out.print(decimal + " = 0b");
         for (; 1 < decimal; ) {
             int div = decimal % 2;
             decimal = decimal / 2;
@@ -21,6 +21,27 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[array.length - 1 - i]);
         }
-
+        */
+        Scanner sc = new Scanner(System.in);
+        int decimal = sc.nextInt();
+        String s = "";
+        int div = decimal % 8;
+        if (div == 0) {
+            s = "0";
+        } else {
+            for (; 0 < div; ) {
+                if (div == 1) {
+                    s += Integer.toString(div);
+                    break;
+                }
+                int b = div % 8;
+                div = div / 8;
+                s += Integer.toString(b);
+           }
+        }
+        char[] array = s.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[array.length - 1 - i]);
+        }
     }
 }
